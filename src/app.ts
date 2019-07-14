e16e71acada1b6ebae5f39fbb22e4c46c09752c5
Peder Korsveien <peder.korsveien@nav.no>
2019-07-14 15:22:46 +0200

    Add ts

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const CLIENT_ID = `${process.env.WITHINGS_CLIENT_ID}`;
+const CLIENT_SECRET = `${process.env.WITHINGS_CLIENT_SECRET}`;
+const REDIRECT_URI = `https://pederpus.no/auth/callback`;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/auth/callback", async (req: Request, res: Response) => {
+  const code = req.params.code;
+  const state = req.params.state;
+  const url = `https://account.withings.com/oauth2/token?grant_type=authorization_code&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}&redirect_uri=${REDIRECT_URI}`;
+  const response = await fetch(url, {
+    method: "POST"
+  });
+
+  response.json();
+
+  return response;
+});
+
+app.get("/auth", (req, res) => {
+  const url = `https://account.withings.com/oauth2_user/authorize2?response_type=code&client_id=${CLIENT_ID}&state=foo&scope=user.metrics&redirect_uri=${REDIRECT_URI}`;
+  res.redirect(url);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
