d29d284a2754e1f6792adb59f556cd92deafb9dd
Peder Korsveien <peder.korsveien@nav.no>
2019-07-14 11:28:26 +0200

    Add auth

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/auth/callback', (req, res) => res.send('Hello'))
+app.get('/auth', (req, res) => {
+    const url = `https://account.withings.com/oauth2_user/authorize2?response_type=code&client_id=${process.env.WITHINGS_CLIENT_ID}&state=foo&scope=user.metrics&redirect_uri=https://pederpus.no/auth/callback`
+    res.redirect(url)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
